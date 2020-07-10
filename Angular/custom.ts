2805536ee00e4f122b8577c63af083bce7517e97
Muqriz <muqriz@favoriot.com>
2020-07-10 15:13:57 +0800

    haha fixed logic

--- Angular/custom.ts
+++ Angular/custom.ts
@@ -4,15 +4,15 @@ class Custom {
     }
 
     // iterate reactive form and add on props and values into object stack
-    public iterateReactiveForm(object, stack = {}) {
+    public iterateReactiveForm(object) {
         const keys = Object.keys(object);
+        let stack = {};
 
         for (let k = 0; k < keys.length; k++) {
             if (!('controls' in object[keys[k]])) {
-                console.log(object[keys[k]].value);
                 stack[keys[k]] = object[keys[k]].value;
             } else {
-                this.iterateReactiveForm(object[keys[k]].controls, stack);
+                stack[keys[k]] = object[keys[k]].value;
             }
         }
 
