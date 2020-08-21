cf09ed52c1a293305ee543c70b57ade5ad85ffff
Muqriz <muqriz@favoriot.com>
2020-08-21 11:37:25 +0800

    added malaysia states

---
+++ JS/malaysia-states.js
@@ -0,0 +1,62 @@
+export const allStates = [
+    {
+        name: 'Johor',
+        val: 'JHR'
+    },
+    {
+        name: 'Kedah',
+        val: 'KDH'
+    },
+    {
+        name: 'Kelantan',
+        val: 'KTN'
+    },
+    {
+        name: 'Melaka',
+        val: 'MLK'
+    },
+    {
+        name: 'Negeri Sembilan',
+        val: 'NSN'
+    },
+    {
+        name: 'Pahang',
+        val: 'PHG'
+    },
+    {
+        name: 'Perak',
+        val: 'PRK'
+    },
+    {
+        name: 'Perlis',
+        val: 'PLS'
+    },
+    {
+        name: 'Pulau Pinang',
+        val: 'PNG'
+    },
+    {
+        name: 'Sabah',
+        val: 'SBH'
+    },
+    {
+        name: 'Terengganu',
+        val: 'TRG'
+    },
+    {
+        name: 'Selangor',
+        val: 'SGR'
+    },
+    {
+        name: 'W.P. Kuala Lumpur',
+        val: 'KUL'
+    },
+    {
+        name: 'W.P. Labuan',
+        val: 'LBN'
+    },
+    {
+        name: 'W.P. Putrajaya',
+        val: 'PJY'
+    },
+];
